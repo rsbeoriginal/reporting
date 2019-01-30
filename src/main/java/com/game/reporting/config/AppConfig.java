dcb2c7571055b861e3a31ef7ad5a46affe5549d8
rishi <rishisharma7361@gmail.com>
2019-01-30 11:58:05 +0530

    reporting project setup and swagger

---
+++ src/main/java/com/game/reporting/config/AppConfig.java
@@ -0,0 +1,26 @@
+package com.game.reporting.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class AppConfig {
+
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.game.reporting.controller"))
+                .paths(regex("/*.*"))
+                .build();
+//                .apiInfo(metaData());
+    }
+}
